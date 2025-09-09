bca3333fe0b35efaf96eeadceac19a5823955665
CarlosMendozaGarcia <mendozagarcia@uninorte.edu.co>
2025-09-08 21:04:40 -0500

    Init Repository

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <section className={styles.welcome_section}>
+
+      </section>
+      
+      <section className={styles.Aboutme_section}>
+
+      </section>
+      
+      <section className={styles.Technologies_section}>
+
+      </section>
+      
+      <section className={styles.Projects_section}>
+
+      </section>
+    </div>
+  );
+}
